45fa39f3009fa551756c73feaa21da54adb647b2
xiaoketeng <1531464644@com>
2018-12-03 14:26:32 +0800

    add

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <div class="tab">
+      <div class="tab-item">商品</div>
+      <div class="tab-item">评论</div>
+      <div class="tab-item">商家</div>
+    </div>
+  </div>
+</template>
+
+<script>
+  // 引入
+import Header from './components/header/header.vue';
+
+export default {
+  name: 'app',
+  components: {
+    Header
+  }
+};
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  #app{
+    .tab{
+      display:flex;
+      width:100%;
+      height:40px;
+      line-height 40px;
+      .tab-item{
+        flex:1;
+        text-align:center;
+      }
+
+    }
+  }
+</style>
